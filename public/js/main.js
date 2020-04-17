cf5f170725719bbc465a9b9519dc1385199869ad
markrajk <marko88rajkovic@gmail.com>
2020-04-17 16:06:28 +0200

    match with xD

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-const menuToggle = document.querySelectorAll('.menu-toggle');
-const nav = document.querySelector('.nav');
-const burger = nav.querySelector('.burger');
+var menuToggle = document.querySelectorAll('.menu-toggle');
+var nav = document.querySelector('.nav');
+var burger = nav.querySelector('.burger');
+
+console.log('HELLO IE!');
 
 function addEvent(element, evName, fn) {
   if (element.addEventListener) {
@@ -27,10 +29,27 @@ function addEvent(element, evName, fn) {
 //   });
 // });
 
-menuToggle.forEach((e) => {
-  addEvent(e, 'click', function () {
+// menuToggle.forEach(function (e) {
+//   addEvent(e, 'click', function () {
+//     nav.style.transition = 'transform .5s ease-in-out';
+//     setTimeout(function () {
+//       nav.style.transition = 'none';
+//     }, 500);
+//     if (nav.classList.contains('onscreen')) {
+//       window.scrollTo(0, 0);
+//       nav.classList.remove('onscreen');
+//       burger.innerHTML = '<i class="fas fa-bars show-xsm"></i>';
+//     } else {
+//       nav.classList.add('onscreen');
+//       burger.innerHTML = '<i class="fas fa-times show-xsm"></i>';
+//     }
+//   });
+// });
+
+for (let i = 0; i < menuToggle.length; i++) {
+  addEvent(menuToggle[i], 'click', function () {
     nav.style.transition = 'transform .5s ease-in-out';
-    setTimeout(() => {
+    setTimeout(function () {
       nav.style.transition = 'none';
     }, 500);
     if (nav.classList.contains('onscreen')) {
@@ -42,4 +61,4 @@ menuToggle.forEach((e) => {
       burger.innerHTML = '<i class="fas fa-times show-xsm"></i>';
     }
   });
-});
+}
